c0fe7e78967714b2a9784e3e27eb4f27329d4c19
Dmitry Davydov <davidov.dima@gmail.com>
2020-10-22 23:51:14 +0300

    image slider animation

---
+++ SocialApp/Controllers/ImageViewController.swift
@@ -0,0 +1,172 @@
+//
+//  ImageViewController.swift
+//  SocialApp
+//
+//  Created by Дима Давыдов on 22.10.2020.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+
+    var animation = UIViewPropertyAnimator()
+    var imageViewList: [UIImageView] = []
+    var currentImagePosition = 0
+    var initialGuesturePoint: CGPoint?
+    
+    enum AnimateTo {
+        case left, right
+    }
+    
+    private func createImageView(_ image: UIImage) {
+        
+        let frame = CGRect(x: imageViewList.count == 0 ? 0 : self.view.frame.size.width, y: self.view.frame.origin.y, width: self.view.frame.width, height: self.view.frame.height)
+        
+        let imageView = UIImageView(frame: frame)
+        imageView.image = image
+        imageView.backgroundColor = .black
+        imageView.contentMode = .scaleAspectFit
+        imageView.autoresizesSubviews = true
+        imageView.clipsToBounds = true
+        imageView.isUserInteractionEnabled = true
+        
+        imageViewList.append(imageView)
+        
+        self.view.addSubview(imageView)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBarController?.tabBar.alpha = 0
+        self.view.backgroundColor = .black
+        
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(panGesture))
+        self.view.addGestureRecognizer(panGestureRecognizer)
+        
+        addImages()
+    }
+    
+    private func isSwipingLeft() -> Bool {
+        let currentImageView = self.imageViewList[self.currentImagePosition]
+        return currentImageView.frame.origin.x > self.view.frame.size.width / 2
+    }
+    private func isSwipingRight() -> Bool {
+        let currentImageView = self.imageViewList[self.currentImagePosition]
+        return currentImageView.frame.origin.x < (self.view.frame.size.width / 2) * -1
+    }
+    
+    private func hasImageOnTheLeft() -> Bool {
+        return currentImagePosition > 0
+    }
+    
+    private func hasImageOnTheRight() -> Bool {
+        return currentImagePosition < imageViewList.count - 1
+    }
+    
+    @objc func panGesture(_ recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            self.initialGuesturePoint = recognizer.translation(in: self.view)
+        case .changed:
+            let currentImageView = self.imageViewList[self.currentImagePosition]
+            let translation = recognizer.translation(in: self.view)
+            
+            currentImageView.frame.origin.x = initialGuesturePoint!.x + (translation.x * 1.6)
+            
+        case .ended:
+
+            if isSwipingLeft() && hasImageOnTheLeft() {
+                animate(.left)
+                return
+            }
+            
+            if isSwipingRight() && hasImageOnTheRight() {
+                animate(.right)
+                return
+            }
+            
+            // default animation
+            
+            animation = UIViewPropertyAnimator(duration: 0.1, curve: .easeInOut, animations: {
+                let currentImageView = self.imageViewList[self.currentImagePosition]
+                currentImageView.frame.origin.x = 0
+            })
+            animation.startAnimation()
+            
+        default:
+            print("1")
+        }
+    }
+    
+    func animate(_ to: AnimateTo) {
+        
+        let currentImageView = self.imageViewList[self.currentImagePosition]
+        
+        var currentViewTargetXPosition: CGFloat
+        var nextView: UIImageView
+        var nextImagePosition = currentImagePosition
+        
+        let xPosition = self.view.frame.width - (0.8 * self.view.frame.width)
+        
+        switch to {
+        case .left:
+            currentViewTargetXPosition = self.view.frame.size.width
+            nextView = self.imageViewList[self.currentImagePosition - 1]
+            nextImagePosition -= 1
+        case .right:
+            currentViewTargetXPosition = self.view.frame.size.width * -1
+            nextView = self.imageViewList[self.currentImagePosition + 1]
+            nextImagePosition += 1
+        }
+        
+        nextView.transform = nextView.transform.scaledBy(x: 0.5, y: 0.5)
+        nextView.alpha = 0.3
+        
+        UIView.animateKeyframes(withDuration: 0.7, delay: 0, options: .calculationModeLinear, animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 0.25, animations: {
+                currentImageView.frame.origin.x = currentViewTargetXPosition
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.25, relativeDuration: 0.5, animations: {
+                nextView.frame.origin.x = xPosition
+            })
+            UIView.addKeyframe(withRelativeStartTime: 0.75, relativeDuration: 0.25, animations: {
+                nextView.transform = .identity
+                nextView.frame.origin.x = 0
+                nextView.alpha = 1
+            })
+        }, completion: {[unowned self] (_) in
+            self.currentImagePosition = nextImagePosition
+        })
+    }
+    
+    func addImages() {
+        
+        let url = URL.init(string: "https://picsum.photos/600")!
+        
+        for _ in 0..<5 {
+            
+            DispatchQueue.global(qos: .userInteractive).async { [unowned self] in
+                
+                let imageData = try? Data(contentsOf: url)
+                
+                DispatchQueue.main.async { [unowned self] in
+                    self.createImageView(UIImage(data: imageData!)!)
+                }
+            }
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        for v in self.view.subviews {
+            v.removeFromSuperview()
+        }
+        self.tabBarController?.tabBar.alpha = 1
+        
+    }
+
+}
