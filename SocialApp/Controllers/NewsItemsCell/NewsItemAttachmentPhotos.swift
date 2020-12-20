3de483e492f042ca0e766dfa82ceda989f676224
Dmitry Davydov <davidov.dima@gmail.com>
2020-12-20 22:12:21 +0300

    feed photos displaying

--- SocialApp/Controllers/NewsItemsCell/NewsItemAttachmentPhotos.swift
+++ SocialApp/Controllers/NewsItemsCell/NewsItemAttachmentPhotos.swift
@@ -15,34 +15,73 @@ protocol AttachmentViewDelegate: class {
 
 class NewsItemAttachmentPhotos: AttachmentViewDelegate {
     weak var view: UIView!
-    var attachment: AttachmentPhoto
+    var attachment: [AttachmentPhoto]
     var subview: UIView?
     
     
-    internal init(view: UIView, attachment: AttachmentPhoto) {
+    internal init(view: UIView, attachment: [AttachmentPhoto]) {
         self.view = view
         self.attachment = attachment
     }
     
     
     func render(){
-        if let url = URL(string: attachment.sizes[0].url),
-           let data = try? Data(contentsOf: url),
-           let img = UIImage(data: data) {
-            
-            subview = UIImageView(image: img)
-            
-            subview?.frame = CGRect(x: 0, y: 0, width: attachment.sizes[0].width, height: attachment.sizes[0].height)
-            
-            
-            view.frame = CGRect(x: view.frame.origin.x, y: view.frame.origin.y, width: view.frame.width, height: CGFloat(attachment.sizes[0].height))
-            view.translatesAutoresizingMaskIntoConstraints = false
-            view.heightAnchor.constraint(equalToConstant: CGFloat(attachment.sizes[0].height)).isActive = true
-            
-            view.addSubview(subview!)
-            
-            
+        
+        if attachment.count == 1 {
+            NewsItemAttachmentPhoto(view: view, attachment: attachment.first!).render()
+            return
         }
+        
+        // 2
+        // 3
+        // 2
+        
+        var attachmentsPhoto = attachment
+        
+        
+        let rowsCount: Int = Int(ceil(Double(attachment.count) / Double(3)))
+        
+        let mainStack = UIStackView()
+        mainStack.alignment = .fill
+        mainStack.distribution = .fillProportionally
+        mainStack.axis = .vertical
+        mainStack.spacing = 5
+        
+        for _ in 0..<rowsCount {
+            let st = UIStackView()
+            st.alignment = .fill
+            st.distribution = .fillProportionally
+            st.axis = .horizontal
+            st.spacing = 5
+            
+            for _ in 0..<3 {
+                if attachmentsPhoto.count == 0 { break }
+                let photo = attachmentsPhoto.removeFirst()
+                 
+                guard let thumb = photo.sizes.find(by: .o),
+                      let url = URL(string: thumb.url),
+                      let data = try? Data(contentsOf: url),
+                      let img = UIImage(data: data) else { continue }
+                
+                let imgView = UIImageView(image: img)
+                imgView.clipsToBounds = true
+                imgView.contentMode = .scaleAspectFill
+                
+                st.addArrangedSubview(imgView)
+            }
+            
+            mainStack.addArrangedSubview(st)
+        }
+        
+        subview = mainStack
+        
+        subview?.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mainStack)
+        
+        subview?.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        subview?.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        subview?.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        subview?.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
     
     func clear() {
